79d2b8c5f8d6b8de67b37ba8bffd23c5537301e8
sathieshs <sathiesh157@hotmail.com>
2022-01-07 18:07:12 +0530

    Update collectfiles.py

--- utils/collectfiles.py
+++ utils/collectfiles.py
@@ -19,15 +19,34 @@ def get_all_file_paths(directory):
     return file_paths
 
 
-def zip_files(sourcedir=os.getcwd(),targetdir=os.getcwd(),target_file_name='code.zip',create_subfolders=True,allowed_extn=[],blacklisted_extn=[]):
+def zip_files(sourcedir=os.getcwd(),targetdir=os.getcwd(),target_file_name='ziped_data.zip',create_subfolders=True,allowed_extn=[],blacklisted_extn=[],blacklisted_files=[]):
     file_paths = get_all_file_paths(sourcedir)
     with ZipFile(os.path.join(targetdir,target_file_name), 'w') as zipObj:
        # Iterate over all the files in directory
        for file in file_paths:
         if(basename(file).split('.')[-1] in allowed_extn or len(allowed_extn)==0 or allowed_extn is None ):
             if(not (basename(file).split('.')[-1] in blacklisted_extn)):
-                   # Add file to zip
-                if create_subfolders:
-                    zipObj.write(file, file)
-                else:
-                    zipObj.write(file, basename(file))
+                if(not(basename(file) in blacklisted_files)):
+                       # Add file to zip
+                    if create_subfolders:
+                        zipObj.write(file, file)
+                    else:
+                        zipObj.write(file, basename(file))
+#zip_files(sourcedir=r'C:\Users\sathi\Desktop\coursera\tensrflow advanced techniques',create_subfolders=True,blacklisted_extn=['mp4','download'])
+
+
+def createfolder_with_files(**kwargs):
+    zip_files(**kwargs)
+    foldername=os.getcwd()
+    for key, value in kwargs.items():
+        if key=='targetdir':
+            foldername=value
+    with ZipFile(os.path.join(foldername,'ziped_data.zip'), 'r') as zip:
+        # printing all the contents of the zip file
+        zip.printdir()
+        # extracting all the files
+        print('Extracting all the files now...')
+        zip.extractall(os.path.join(foldername,'ziped_data_folder'))
+        print('Extraction done')
+            
+#createfolder_with_files(sourcedir=r'C:\Users\sathi\Desktop\coursera\tensrflow advanced techniques',create_subfolders=True,blacklisted_extn=['mp4','download'],blacklisted_files=['Untitled-checkpoint.ipynb','Untitled.ipynb'])    
